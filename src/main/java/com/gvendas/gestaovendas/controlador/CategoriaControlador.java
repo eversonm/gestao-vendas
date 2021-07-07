188a60e92884ec0ec438679cc9bb6f48ccc7c250
Everson Magalhães <everson4774@gmail.com>
2021-07-07 11:25:45 -0300

    Documentando API com Swagger 2

--- src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -2,6 +2,8 @@ package com.gvendas.gestaovendas.controlador;
 
 import com.gvendas.gestaovendas.entidades.Categoria;
 import com.gvendas.gestaovendas.servico.CategoriaServico;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@Api(tags = "Categoria")
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaControlador {
@@ -18,23 +21,27 @@ public class CategoriaControlador {
     @Autowired
     private CategoriaServico categoriaServico;
 
+    @ApiOperation(value = "Listar")
     @GetMapping
     public List<Categoria> listarTodas(){
         return categoriaServico.listarTodas();
     }
 
+    @ApiOperation(value = "Listar por código")
     @GetMapping("/{codigo}")
     public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
         Optional<Categoria> categoria = categoriaServico.buscarPorCodigo(codigo);
         return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
     }
 
+    @ApiOperation(value = "Salvar")
     @PostMapping()
     public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria){
         Categoria categoriaSalva =  categoriaServico.salvar(categoria);
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
+    @ApiOperation(value = "Atualizar")
     @PutMapping("/{codigo}")
     public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria){
         return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));
